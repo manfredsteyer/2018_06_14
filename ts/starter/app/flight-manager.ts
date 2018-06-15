831a9ff155e896ad17fdb485d61818301401b92f
ManfredSteyer <manfred.steyer@gmx.net>
2018-06-15 12:53:23 +0200

    final

--- ts/starter/app/flight-manager.ts
+++ ts/starter/app/flight-manager.ts
@@ -1,5 +1,10 @@
 import { Flight, IFlight } from "./flights";
 
+import { Observable, Observer, of, interval } from 'rxjs';
+import { switchMap, startWith } from 'rxjs/operators';
+
+// type SuccessFunction = (flights: Flight[]) => void;
+// type FailFunction = (err:any) => void;
 
 export abstract class AbstractFlightManager {
 
@@ -25,6 +30,10 @@ export class FlightManager extends AbstractFlightManager  {
         this.cache[idx] = flight;
     }
 
+    clearCache() {
+        this.cache = [];
+    }
+
     where(condition: FlightCondition): Array<IFlight> {
         let result = new Array<IFlight>();
         for (let flight of this.cache) {
@@ -45,6 +54,94 @@ export class FlightManager extends AbstractFlightManager  {
         return result;
     }
 
+
+    public load(from: string, to: string): Promise<IFlight[]> {
+
+        return new Promise((resolve: Function, reject: Function) => {
+
+            let xmlhttp = new XMLHttpRequest();
+    
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.debug(this.responseText);
+                    resolve(JSON.parse(this.responseText));
+                    
+                }
+                else if (this.readyState == 4 && this.status >= 400) {
+                    console.error('error loading flights')
+                    reject(xmlhttp);
+                }
+                else if (this.readyState == 4) {
+                    console.error('unexpected state!');
+                    reject(xmlhttp);
+                }
+            };
+        
+            let url = `http://www.angular.at/api/flight?from=${encodeURIComponent(from)}&to=${encodeURIComponent(to)}`;
+    
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+
+        })
+    }
+
+
+    public observeFlights(from: string, to: string): Observable<IFlight[]> {
+        return interval(2000).pipe(
+            startWith(0),
+            switchMap(i => this.createData(from, to)
+        ))
+    }
+
+    private createData(from: string, to: string): Observable<IFlight[]> {
+        if (this.cache.length == 0) {
+            return this.createObservable(from, to);
+        }
+        else {
+            return this.changeFlights();
+        }
+    }
+
+
+
+    private changeFlights(): Observable<IFlight[]> {
+        let idx = Math.floor(this.cache.length * Math.random());
+        this.cache[idx].price += 100;
+        return of(this.cache);
+    }
+
+    private createObservable(from: string, to: string): Observable<IFlight[]> {
+
+        return Observable.create((sender: Observer<IFlight[]>) => {
+
+            let xmlhttp = new XMLHttpRequest();
+    
+            xmlhttp.onreadystatechange = () => {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    this.cache = JSON.parse(xmlhttp.responseText); 
+                    
+                    this.cache.forEach(f => f.price = 300);
+
+                    sender.next(this.cache);
+                }
+                else if (xmlhttp.readyState == 4 && xmlhttp.status >= 400) {
+                    console.error('error loading flights')
+                    sender.error(JSON.parse(xmlhttp.responseText));
+                }
+                else if (xmlhttp.readyState == 4) {
+                    console.error('unexpected state!');
+                    sender.error(xmlhttp);
+                }
+            };
+        
+            let url = `http://www.angular.at/api/flight?from=${encodeURIComponent(from)}&to=${encodeURIComponent(to)}`;
+    
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+
+        })
+
+    }
 }
 
 export class DummyFlightManager extends AbstractFlightManager {
@@ -60,6 +157,8 @@ export class DummyFlightManager extends AbstractFlightManager {
     public where(condition: FlightCondition): Array<IFlight> {
         return [];
     }
+
+
 }
 
 
--- ts/starter/app/main.ts
+++ ts/starter/app/main.ts
@@ -1,8 +1,9 @@
 import { Flight, IFlight, CharterFlight, IBus, FlightCategory } from './flights';
 import { ExtFlightManager } from './ext-flight-manager';
-import { createFlightManager } from './flight-manager';
+import { createFlightManager, FlightManager } from './flight-manager';
 import { Invoice } from './invoice';
 import { sayHello } from './hello';
+import { Subscription } from 'rxjs';
 
 let f1: IFlight = new Flight(1);
 f1.from = 'Graz';
@@ -25,11 +26,25 @@ f3.category = FlightCategory.Cargo;
 
 let cache = [f1, f2, f3];
 
-//let fm = new ExtFlightManager(cache);
-let fm = createFlightManager(cache);
+let fm = new FlightManager(cache);
+//let fm = createFllightManager(cache);
 
 // let result = fm.search('Graz', 'Mallorca');
 let result = fm.where(f => f.from == 'Graz' || f.to == 'Graz');
+
+fm.load('Graz', 'Hamburg')
+  .then(flights => { 
+      console.debug('got flights', flights); 
+
+      return fm.load('Hamburg', 'Graz');
+    })
+    .then(flights => { 
+        console.debug('got return flights', flights); 
+  
+   })
+  .catch(err => {
+    console.error('got error', err);
+  })
 
 console.debug('result', result);
 // console.debug('count of flights', fm.count);
@@ -41,4 +56,57 @@ console.debug('result', result);
  let invoice2 = new Invoice<CharterFlight>(f3);
  invoice2.printInvoice();
 
- sayHello(undefined, 'Meier');
+
+ let subscription: Subscription = null;
+
+ function search(): void {
+
+    if (subscription) {
+        subscription.unsubscribe();
+    }
+
+    let from = $('input[name=from]');
+    let to = $('input[name=to]');
+
+    let fromValue = from.val() as string;
+    let toValue = to.val() as string;
+
+    // let result = fm.search(fromValue, toValue);
+    // render(result);
+
+    /*
+    fm.load(fromValue, toValue)
+        .then(flights => render(flights))
+        .catch(err => console.error('got error', err));
+    */
+
+    fm.clearCache();
+
+    subscription = fm.observeFlights(fromValue, toValue).subscribe(
+        flights => render(flights),
+        err => console.error('got error', err)
+    );
+
+ }
+
+ function render(result: IFlight[]): void {
+    let html = '<table class="table table-striped">';
+
+    for(let f of result) {
+        html += `
+            <tr>
+                <td>${f.id}</td>
+                <td>${f.from}</td>
+                <td>${f.to}</td>
+                <td>${f.date}</td>
+                <td>${f.price}</td>
+            </tr>
+        `;
+    }
+
+    html += '</table>';
+
+    $("#placeholder").html(html)
+ }
+
+ $('#btnSearch').click(search);
